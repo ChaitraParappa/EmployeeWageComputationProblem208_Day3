b7a5b50d9faaa81573279fcee1ec950b4b531976
Chaitra P <chaitra.p@bp.com>
2022-10-03 09:36:35 +0530

    Calculate employee wage

--- EmplyeeCheck.cs
+++ EmplyeeCheck.cs
@@ -16,19 +16,33 @@ namespace EmployeeWageProblem
         /// </summary>
         public static void EmployeeAttendence()
         {
+            //Constants
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
 
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
+
+            //Comutation
             Random random = new Random();
             int empCheck = random.Next(2);
 
             if(empCheck==IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is full time");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Employee is no worked");
+                empHrs = 0;
             }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
+            Console.WriteLine("EmpWage : " + empWage);
         }
     }
 }
