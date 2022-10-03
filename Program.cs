97fc9ccbc4ec369a13e055f76a2a705a658199eb
Chaitra P <chaitra.p@bp.com>
2022-10-03 09:10:02 +0530

    added uc1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computataion Problem");
+
+            EmplyeeCheck.EmployeeAttendence();
         }
     }
 }
